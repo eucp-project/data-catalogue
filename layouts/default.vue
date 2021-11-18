a1ef51971cf2886f8f38e83dd0d6682cdbef2fe5
Peter Kalverla <peter.kalverla@gmx.com>
2021-11-18 17:27:12 +0100

    add layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-screen w-screen p-4">
+    <div class="flex gap-10 m-2 items-center">
+      <NuxtLink :to="`/`">
+        <img src="eucp_logo.png" alt="EUCP Logo">
+      </NuxtLink>
+      <h1 class="text-2xl">
+        Data Catalogue
+      </h1>
+    </div>
+    <Nuxt />
+  </div>
+</template>
