9cd41861b39e8709cb97a88404b17958dff5f0e9
Peter Kalverla <peter.kalverla@gmx.com>
2021-11-18 17:28:38 +0100

    Basic setup for index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <Tutorial />
+  <!-- Buttons -->
+  <div class="flex flex-col place-content-evenly h-full">
+    <div class="flex gap-48 place-content-center">
+      <NuxtLink to="cpm">
+        <div class="border-2 p-8 rounded-lg text-3xl bg-red-400">
+          CPM data
+        </div>
+      </NuxtLink>
+      <NuxtLink to="decadal">
+        <div class="border-2 p-8 rounded-lg text-3xl bg-green-400">
+          Decadal
+        </div>
+      </NuxtLink>
+      <NuxtLink to="topical">
+        <div class="border-2 p-8 rounded-lg text-3xl bg-blue-400">
+          Topical
+        </div>
+      </NuxtLink>
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
