1282b2d13c60603042859eac4377b64876c3481a
Peter Kalverla <peter.kalverla@gmx.com>
2021-12-16 15:24:54 +0100

    Cpm in context  (#15)
    * Add vega, vega-lite, and vega-embed
    
    * Insert vega-chart showing cpm data in context
    
    * Link ancestors; highlight all parents if clicked outside
    
    * Fixed jitter

---
+++ components/DataInContext.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="p-8">
+    <div id="vis" />
+  </div>
+</template>
+
+<script>
+import vegaEmbed from 'vega-embed'
+
+export default {
+  data () {
+    return {
+      view: {},
+      data: [],
+      spec: {}
+    }
+  },
+  async mounted () {
+    this.data = await fetch('cpm_context_data.json').then(res => res.json())
+    this.addManualJitter()
+    this.spec = await fetch('context_chart_spec.json').then(res => res.json())
+
+    // Link the plotted data to an object we can access in JS
+    delete this.spec.data.url
+    this.spec.data.name = 'myData'
+    this.spec.datasets = { myData: this.data }
+
+    this.view = await vegaEmbed('#vis', this.spec).then(result => result.view).catch()
+    this.view.addEventListener('click', this.updateSelection)
+  },
+  methods: {
+    updateSelection (event, item) {
+      if (item.datum.model) {
+        this.data.forEach((datum) => {
+          if ((datum.model === item.datum.model) & (datum.project === item.datum.project)) {
+            // without comparing project, UKCP rcm and gcm are linked due to their numbered model names
+            datum.highlight = true
+          } else if (item.datum.ancestors.includes(datum.model)) {
+            datum.highlight = true
+          } else {
+            datum.highlight = false
+          }
+        })
+      } else {
+        this.data.forEach((datum) => {
+          datum.highlight = datum.isparent
+        })
+      }
+      this.pushNewDataToChart()
+    },
+    pushNewDataToChart () {
+      // this.view.data('myData', this.data).run() // only works from vega v5.5 onwards
+      this.view.remove('myData', d => true).run() // remove all previous entries
+      this.view.insert('myData', this.data).run()
+      this.view.resize().run()
+    },
+    addManualJitter () {
+      // Workaroud to fix xloc for data when view is re-rendered.
+      this.data.forEach((datum) => {
+        // Generate Gaussian jitter with a Box-Muller transform
+        datum.xloc = 5 * Math.sqrt(-2 * Math.log(Math.random())) * Math.cos(2 * Math.PI * Math.random())
+      })
+    }
+  }
+}
+</script>
