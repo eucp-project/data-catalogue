41324c04980b94cddd0d94f3763d7214c5fbf671
Yang <y.liu@esciencecenter.nl>
2022-01-13 17:15:13 +0100

    add modal component

---
+++ components/ModalDialog.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="modal-overlay">
+    <div class="modal">
+      <h6>Dataset info</h6>
+      <p>Display details of dataset</p>
+      <button @click="$emit('close-modal')">
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  background-color: #000000da;
+}
+
+.modal {
+  text-align: center;
+  background-color: white;
+  height: 500px;
+  width: 500px;
+  margin-top: 10%;
+  padding: 60px 0;
+  border-radius: 20px;
+}
+
+h6 {
+  font-weight: 500;
+  font-size: 28px;
+  margin: 20px 0;
+}
+
+p {
+  font-size: 16px;
+  margin: 20px 0;
+}
+
+button {
+  background-color: #ac003e;
+  width: 150px;
+  height: 40px;
+  color: white;
+  font-size: 14px;
+  border-radius: 16px;
+  margin-top: 50px;
+}
+</style>
