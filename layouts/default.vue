2387fdf1a072e113d8ab5369c2e5f9c9b214d39c
Yang <y.liu@esciencecenter.nl>
2022-02-14 09:42:16 +0100

    switch off breadcrumbs feature on homepage

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@
         Data Catalogue
       </h1>
       <div class="m-4 ml-8 text-lg">
-        <Breadcrumbs :crumbs="crumbs" />
+        <Breadcrumbs v-if="crumbs[1] !== ``" :crumbs="crumbs" />
       </div>
     </div>
     <Nuxt />
