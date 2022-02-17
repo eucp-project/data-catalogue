50dac69028a9ca07fdf7d51067b8cb668b921dad
Yang <y.liu@esciencecenter.nl>
2022-02-17 21:21:28 +0100

    create and style buttons for navigation in the default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,13 +4,19 @@
       <NuxtLink :to="`/`">
         <img src="eucp_logo.png" alt="EUCP Logo">
       </NuxtLink>
-      <div>
+      <div class="flex-l">
         <h1 class="text-4xl">
           Data Catalogue
         </h1>
         <Breadcrumbs class="mt-2" />
       </div>
+      <div class="ml-auto gap-10 m-2 mr-10 text-l space-x-5">
+        <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">Decadal</button>
+        <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">CPM</button>
+        <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">Topical</button>
+      </div>
     </div>
+
     <Nuxt class="pt-4" />
   </div>
 </template>
