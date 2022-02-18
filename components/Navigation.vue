70087b701bd315752d834955c5887c0b4e197524
Yang <y.liu@esciencecenter.nl>
2022-02-18 15:44:10 +0100

    Create navigation component and clean up the default

---
+++ components/Navigation.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="text-l">
+    <div
+      class="space-y-0.5"
+      @mouseover="hover = true"
+      @mouseleave="hover = false"
+    >
+      <!-- main button -->
+      <NuxtLink :to="`/` + page.toLowerCase()">
+        <button
+          class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded"
+        >
+          {{ page }}
+        </button>
+      </NuxtLink>
+      <!-- dropdown pages -->
+      <div
+        v-if="hover"
+        class="absolute flex flex-col space-y-0.5"
+      >
+        <div
+          v-for="(subPage, i) in subPages"
+          :key="i"
+        >
+          <NuxtLink :to="`/` + page.toLowerCase() + `#` + subPage.toLowerCase()">
+            <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
+              {{ subPage }}
+            </button>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    page: {
+      type: String,
+      default: null,
+      required: false
+    },
+    subPages: {
+      type: Array,
+      default: null,
+      required: false
+    }
+  },
+  data () {
+    return {
+      hover: false
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -10,70 +10,10 @@
         </h1>
         <Breadcrumbs class="mt-2" />
       </div>
-      <div class="flex ml-auto gap-10 m-2 mr-10 text-l space-x-1">
-        <div
-          class="space-y-0.5"
-          @mouseover="hoverCPM = true"
-          @mouseleave="hoverCPM = false"
-        >
-          <NuxtLink :to="`/cpm`">
-            <button
-              class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded"
-            >
-              CPM
-            </button>
-          </NuxtLink>
-          <div
-            v-if="hoverCPM"
-            class="absolute flex flex-col space-y-0.5"
-          >
-            <NuxtLink :to="`/cpm#overview`">
-              <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-                Overview
-              </button>
-            </NuxtLink>
-            <NuxtLink :to="`/cpm#storyboards`">
-              <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-                Storyboards
-              </button>
-            </NuxtLink>
-            <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-              Demonstrator
-            </button>
-          </div>
-        </div>
-        <div
-          class="space-y-0.5"
-          @mouseover="hoverDecadal = true"
-          @mouseleave="hoverDecadal = false"
-        >
-          <NuxtLink :to="`/decadal`">
-            <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-              Decadal
-            </button>
-          </NuxtLink>
-          <div
-            v-if="hoverDecadal"
-            class="absolute flex flex-col space-y-0.5"
-          >
-            <NuxtLink :to="`/decadal#forecasts`">
-              <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-                Forecasts
-              </button>
-            </NuxtLink>
-            <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-              Hindcasts
-            </button>
-            <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-              Experiments
-            </button>
-          </div>
-        </div>
-        <div>
-          <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-            Topical
-          </button>
-        </div>
+      <div class="flex ml-auto gap-10 m-2 mr-10 space-x-1">
+        <Navigation :page="`CPM`" :sub-pages="pagesCPM" />
+        <Navigation :page="`Decadal`" :sub-pages="pagesDecadal" />
+        <Navigation :page="`Topical`" />
       </div>
     </div>
     <Nuxt class="pt-4" />
@@ -84,8 +24,8 @@
 export default {
   data () {
     return {
-      hoverCPM: false,
-      hoverDecadal: false
+      pagesCPM: ['Overview', 'Storyboards', 'Lines of evidence', 'Demonstrator'],
+      pagesDecadal: ['Forecasts', 'Hindcasts', 'Experiments']
     }
   }
 }
