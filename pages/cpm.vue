8974d60505b95af3c791f0c3e98782703c4411b6
Peter Kalverla <peter.kalverla@gmx.com>
2022-02-18 13:59:43 +0100

    Refactor tab navigation into custom component

--- pages/cpm.vue
+++ pages/cpm.vue
@@ -3,21 +3,7 @@
     <div class="flex gap-4 m-8">
       <CpmMap v-model="domain" />
       <div class="border-4 flex-grow">
-        <div class="flex bg-gray-100">
-          <nav>
-            <ul>
-              <li v-for="page in pages" :key="page.url" class="inline-block">
-                <NuxtLink
-                  role="button"
-                  :to="page.url"
-                  class="text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none"
-                >
-                  {{ page.title }}
-                </NuxtLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <NavTabs :pages="pages" />
         <NuxtChild />
       </div>
     </div>
@@ -40,11 +26,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-a.nuxt-link-exact-active {
-  color: rgb(59 130 246);
-  border-bottom-width: 2px;
-  border-color: rgb(59 130 246);
-}
-</style>
