a7451f320aca5916e349c0b1c2a318b42379706a
Yang <y.liu@esciencecenter.nl>
2022-02-18 14:33:57 +0100

    styling the dropdown list and add nuxt link to each button

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,43 +12,55 @@
       </div>
       <div class="flex ml-auto gap-10 m-2 mr-10 text-l space-x-1">
         <div
+          class="space-y-0.5"
           @mouseover="hoverCPM = true"
           @mouseleave="hoverCPM = false"
         >
-          <button
-            class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded"
-          >
-            CPM
-          </button>
+          <NuxtLink :to="`/cpm`">
+            <button
+              class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded"
+            >
+              CPM
+            </button>
+          </NuxtLink>
           <div
             v-if="hoverCPM"
-            class="absolute flex"
+            class="absolute flex flex-col space-y-0.5"
           >
-            <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-              Overview
-            </button>
-            <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-              Storyboards
-            </button>
+            <NuxtLink :to="`/cpm#overview`">
+              <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
+                Overview
+              </button>
+            </NuxtLink>
+            <NuxtLink :to="`/cpm#storyboards`">
+              <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
+                Storyboards
+              </button>
+            </NuxtLink>
             <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
               Demonstrator
             </button>
           </div>
         </div>
         <div
+          class="space-y-0.5"
           @mouseover="hoverDecadal = true"
           @mouseleave="hoverDecadal = false"
         >
-          <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-            Decadal
-          </button>
+          <NuxtLink :to="`/decadal`">
+            <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
+              Decadal
+            </button>
+          </NuxtLink>
           <div
             v-if="hoverDecadal"
-            class="absolute flex"
+            class="absolute flex flex-col space-y-0.5"
           >
-            <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
-              Forecasts
-            </button>
+            <NuxtLink :to="`/decadal#forecasts`">
+              <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
+                Forecasts
+              </button>
+            </NuxtLink>
             <button class="w-32 bg-blue-300 hover:bg-blue-500 text-white py-2 px-4 rounded">
               Hindcasts
             </button>
