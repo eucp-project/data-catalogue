29bed92547a556328e1157f4b33030e958662565
Peter Kalverla <peter.kalverla@gmx.com>
2022-02-18 15:28:14 +0100

    Reorganize content management of decadal data

--- pages/decadal/_page.vue
+++ pages/decadal/_page.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="flex gap-8 justify-start">
-    <div class="flex flex-col prose">
-      <h2>{{ page.title }}</h2>
-      <nuxt-content :document="page" />
-    </div>
+    <nuxt-content :document="page" class="prose" />
     <div class="flex flex-col">
-      <img :src="page.img" alt="topic picture" style="widht:auto;height:800px;object-fit: contain">
+      <img :src="page.img" alt="topic picture" style="width:auto;height:800px;object-fit: contain">
       <p class="italic">
         {{ page.caption }}
       </p>
