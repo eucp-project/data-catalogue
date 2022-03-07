a876fb7f6848365429177a041a23a7aef44ca709
Yang <y.liu@esciencecenter.nl>
2022-03-07 17:37:31 +0100

    upload more maps for pr and tas

--- python/maps_creator_cpm_atlas.py
+++ python/maps_creator_cpm_atlas.py
@@ -1,32 +1,73 @@
 
 """
-Update content of the CPM Atlas - generate maps based on the CPM data
-Function        : Plot maps in a uniform way based on the CPM data
+Update content of the CPM Atlas - generate maps based on the preprocessed CPM data
+Function        : Plot maps in a uniform way based on the preprocessed CPM data
 Author          : Team BETA
-First Built     : 2021.12.15
-Last Update     : 2021.12.16
-Library         : os, glob, netcdf4, matplotlib, cartopy, argparse
+First Built     : 2022.03.02
+Last Update     : 2022.03.07
+Library         : os, pathlib, itertools, matplotlib, cartopy, xarray, rioxarray, argparse
 Description     : This script serves to extract netcdf data and generate maps
                   for CPM catalogue page.
 Return Values   : png files
-Note            : All the maps are generated in a uniform way.
+Note            : The data is preprocessed and provided by Tom Crocker from Met Office.
 """
 
 import os
 from pathlib import Path
-import argparse
+from itertools import product
+import numpy as np
+import xarray as xr
+# plotting
 import matplotlib
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-from textwrap import wrap
-import xarray as xr
+import cartopy.crs as crs
+from cartopy import feature as cfeature
+# for clipping
+import rioxarray
 # Generate images without having a window appear
 matplotlib.use('Agg')
 
 #####################################################################
 ## This script is intended for batch processing of maps
-## will be complete when the choices of cpm images were made.
 #####################################################################
+
+# function for plotting maps
+def plot_map(dataset, outfile, region, variable):
+    # Plot
+    fig = plt.figure(dpi=120)
+    ax = fig.add_subplot(111, projection=crs.PlateCarree())
+    dataset.plot(ax=ax, cmap="BrBG", cbar_kwargs={'label': ''})
+    # colorbar
+    # cbar = fig.colorbar(im)
+    # cbar.set_label('')
+    # Prettify
+    ax.add_feature(cfeature.OCEAN, zorder=2)
+    ax.coastlines(zorder=3)
+    ax.axis('off')
+    if variable == "pr":
+        ax.set_title('Relative precipitation (%)')
+    elif variable == "tas":
+        ax.set_title('Relative temperature (degC)')
+
+    # Save
+    fig.savefig(outfile)
+    plt.close()
+
+# function for region clipping
+def region_clip(infile, region):
+    # load dataset
+    xds = xr.open_dataset(infile)
+    # drop unused variables, otherwise it will cause 
+    # an error when configuring the coordinate system
+    xds = xds.drop_vars("lat_bnds")
+    xds = xds.drop_vars("lon_bnds")
+    # set-up the coordinate system known to rio
+    xds.rio.write_crs("EPSG:4326", inplace=True)
+    # clip data
+    clipped_data = xds.unknown.rio.clip(regions[region], "EPSG:4326", all_touched=True)
+
+    return clipped_data
+
 
 if __name__ == "__main__":
     # define command line arguments
