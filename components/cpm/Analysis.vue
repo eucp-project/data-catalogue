b56ae8c41e9fcd7147fd5491365d9953ae74c163
Yang <y.liu@esciencecenter.nl>
2022-03-14 11:09:58 +0100

    Explorer cpm data tom (#35)
    * Display maps of future tas and pr based on preprocessed data (https://zenodo.org/record/6012703)
    * Python script for creating those maps
    * Use leaflet map to select domain and update maps accordingly
    * Add dropdowns to select maps from different groups, seasons, etc.

--- components/cpm/Analysis.vue
+++ components/cpm/Analysis.vue
@@ -2,49 +2,147 @@
   <div class="w-full h-full">
     <span class="space-x-3 p-3">
       <CpmDropdown v-model="selectedVariable" :options="variables" alttext="Choose a variable." />
-      <CpmDropdown v-model="selectedResolution" :options="resolution" alttext="Select a resolution." />
-      <CpmDropdown v-model="selectedCategory" :options="categories" alttext="Choose a category" />
-      <CpmDropdown v-model="selectedDate" :options="dates" alttext="Select a date." />
+      <CpmDropdown v-model="selectedSeason" :options="seasons" alttext="Select a season." />
+      <CpmDropdown v-model="selectedModel" :options="models" alttext="Choose a model/group" />
+      <!-- same style but not selectable option to display region -->
+      <select
+        class="border border-gray-300 rounded-full cursor-pointer
+      text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400
+      focus:outline-none appearance-none"
+      >
+        <option :value="domain">
+          {{ domain }}
+        </option>
+      </select>
     </span>
-    <div
-      class="bg-no-repeat bg-left-top bg-contain w-full h-full"
-      :style="{backgroundImage: `url(${bgImage})` }"
-    />
+    <div class="w-full h-full flex flex-wrap">
+      <div class="w-1/3 h-full">
+        <p class="pt-6 text-center text-lg prose">
+          CPM experiment
+        </p>
+        <div
+          class="bg-no-repeat bg-left-top bg-contain w-full h-full"
+          :style="{backgroundImage: `url(${cpmImage})` }"
+        />
+      </div>
+      <div class="w-1/3 h-full">
+        <p class="pt-6 text-center text-lg prose">
+          RCM experiment
+        </p>
+        <div
+          class="bg-no-repeat bg-left-top bg-contain w-full h-full"
+          :style="{backgroundImage: `url(${rcmImage})` }"
+        />
+      </div>
+      <div class="w-1/3 h-full">
+        <p class="pt-6 text-center text-lg prose">
+          GCM experiment
+        </p>
+        <div
+          class="bg-no-repeat bg-left-top bg-contain w-full h-full"
+          :style="{backgroundImage: `url(${gcmImage})` }"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    domain: {
+      default: 'AL',
+      type: String
+    }
+  },
   data () {
     return {
       selectedVariable: 'pr',
-      selectedResolution: '3KM',
-      selectedCategory: 'FREQ',
-      selectedDate: '2005-05-30',
+      selectedSeason: 'DJF',
+      selectedModel: 'CMCC',
       variables: {
-        pr: 'Precipitation'
+        pr: 'Precipitation',
+        tas: 'Temperature'
       },
-      resolution: {
-        '3KM': '3KM',
-        '12KM': '12KM'
+      seasons: {
+        DJF: 'Winter',
+        JJA: 'Summer'
       },
-      categories: {
-        FREQ: 'FREQ',
-        INT: 'INT'
+      models: {
+        CMCC: 'CMCC',
+        CNRM: 'CNRM',
+        ETHZ: 'ETHZ',
+        GERICS: 'GERICS',
+        UKMO: 'UKMO',
+        'DMI/SMHI': 'DMI/SMHI',
+        ICTP: 'ICTP',
+        KNMI: 'KNMI'
       },
-      dates: {
-        '2005-05-30': '2005-05-30',
-        '2005-08-30': '2005-08-30',
-        '2005-11-30': '2005-11-30',
-        '2006-01-01': '2006-01-01'
+      modelsList: {
+        CMCC: {
+          cpm: 'CLMcom-CMCC-CCLM5-0-9',
+          rcm: 'CCLM4-8-17_ICHEC-EC-EARTH',
+          gcm: 'EC-EARTH'
+        },
+        CNRM: {
+          cpm: 'CNRM-AROME41t1',
+          rcm: 'ALADIN63_CNRM-CERFACS-CNRM-CM5',
+          gcm: 'CNRM-CM5'
+        },
+        ETHZ: {
+          cpm: 'COSMO-pompa',
+          rcm: 'CCLM4-8-17_MPI-M-MPI-ESM-LR',
+          gcm: 'MPI-ESM-LR'
+        },
+        GERICS: {
+          cpm: 'GERICS-REMO2015',
+          rcm: 'REMO2015_MPI-M-MPI-ESM-LR',
+          gcm: 'MPI-ESM-LR'
+        },
+        UKMO: {
+          cpm: 'HadREM3-RA-UM10.1',
+          rcm: 'MOHC-HadGEM3-GC3.1-N512_MOHC-HadGEM2-ES',
+          gcm: 'HadGEM2-ES'
+        },
+        'DMI/SMHI': {
+          cpm: 'HCLIMcom-HCLIM38-AROME',
+          rcm: 'HCLIMcom-HCLIM38-ALADIN_ICHEC-EC-EARTH',
+          gcm: 'EC-EARTH'
+        },
+        ICTP: {
+          cpm: 'ICTP-RegCM4-7-0',
+          rcm: 'ICTP-RegCM4-7-0_MOHC-HadGEM2-ES',
+          gcm: 'HadGEM2-ES'
+        },
+        KNMI: {
+          cpm: 'KNMI-HCLIM38h1-AROME',
+          rcm: 'KNMI-RACMO23E_KNMI-EC-EARTH',
+          gcm: 'EC-EARTH'
+        }
       }
     }
   },
   computed: {
-    bgImage () {
-      const fallback = '~/static/cpm_analysis/cpm_prec.png'
+    cpmImage () {
+      const fallback = 'empty.png'
       try {
-        return require('~/static/cpm_analysis/ALP_' + this.selectedVariable + '_' + this.selectedResolution + '_' + this.selectedCategory + '_' + this.selectedDate + '.png')
+        return require('~/static/maps/' + this.domain + '/' + this.selectedVariable + '/' + 'cordex-cpm_' + this.modelsList[this.selectedModel].cpm + '_' + this.selectedSeason + '.png')
+      } catch (err) {
+        return fallback
+      }
+    },
+    rcmImage () {
+      const fallback = 'empty.png'
+      try {
+        return require('~/static/maps/' + this.domain + '/' + this.selectedVariable + '/' + 'CORDEX_' + this.modelsList[this.selectedModel].rcm + '_' + this.selectedSeason + '.png')
+      } catch (err) {
+        return fallback
+      }
+    },
+    gcmImage () {
+      const fallback = 'empty.png'
+      try {
+        return require('~/static/maps/' + this.domain + '/' + this.selectedVariable + '/' + 'CMIP5_' + this.modelsList[this.selectedModel].gcm + '_' + this.selectedSeason + '.png')
       } catch (err) {
         return fallback
       }
--- pages/cpm/analyse.vue
+++ pages/cpm/analyse.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="w-full h-full p-2">
-    <CpmAnalysis />
+    <CpmAnalysis :domain="domain" />
   </div>
 </template>
+
+<script>
+export default {
+  props: ['domain']
+}
+</script>
