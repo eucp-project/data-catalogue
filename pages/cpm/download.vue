e8d9075339ab525bc62c8ab0a9bdc33b5d6b7678
Peter Kalverla <peter.kalverla@gmx.com>
2022-04-27 15:43:56 +0200

    Add cpm download info

--- pages/cpm/download.vue
+++ pages/cpm/download.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <p>download</p>
+  <div class="prose max-w-none">
+    <h1>Data access</h1>
+    <p>
+      We are currently in the process of uploading the data to ESGF. Once the
+      data is available there, we will provide instructions on how to access it.
+    </p>
+
+    <p>
+      We are looking into possibilities of making pre-processed subsets of the data available through zenodo:
+    </p>
+    <ul>
+      <li>Link 1</li>
+      <li>Link 2</li>
+    </ul>
   </div>
 </template>
