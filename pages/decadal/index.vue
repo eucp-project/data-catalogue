4d80965af26278cceff1cb2e6c13b0429b770708
Peter Kalverla <peter.kalverla@gmx.com>
2022-04-27 15:44:11 +0200

    Tweak decadal index page

--- pages/decadal/index.vue
+++ pages/decadal/index.vue
@@ -5,5 +5,11 @@
       EUCP has contributed to the advancement of decadal prediction. This page
       provides an overview of the main data outcomes of this work.
     </p>
+    <p>Use the menu or the following links to learn more about:</p>
+    <ul>
+      <li><NuxtLink to="/decadal/forecasts">Forecasts</NuxtLink></li>
+      <li><NuxtLink to="/decadal/hindcasts">Hindcasts</NuxtLink></li>
+      <li><NuxtLink to="/decadal/experiments">Experiments</NuxtLink></li>
+    </ul>
   </div>
 </template>
