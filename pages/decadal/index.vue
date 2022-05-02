3a23761b22550f1aa3e6d2f00f78beb1258b48dd
Peter Kalverla <peter.kalverla@gmx.com>
2022-05-02 17:23:38 +0200

    Link to C3S sectoral applications

--- pages/decadal/index.vue
+++ pages/decadal/index.vue
@@ -7,9 +7,32 @@
     </p>
     <p>Use the menu or the following links to learn more about:</p>
     <ul>
-      <li><NuxtLink to="/decadal/forecasts">Forecasts</NuxtLink></li>
-      <li><NuxtLink to="/decadal/hindcasts">Hindcasts</NuxtLink></li>
-      <li><NuxtLink to="/decadal/experiments">Experiments</NuxtLink></li>
+      <li>
+        <NuxtLink to="/decadal/forecasts">
+          Forecasts
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/decadal/hindcasts">
+          Hindcasts
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/decadal/experiments">
+          Experiments
+        </NuxtLink>
+      </li>
     </ul>
+    <h4>C3S sectoral applications of decadal predictions</h4>
+    <p>
+      In addition to the outputs shown on here, EUCP partners have collaborated
+      with C3S in a dedicated C3S project to develop demonstrators around
+      sectoral applications of decadal predictions. More information about this
+      can be found on the <a
+        href="https://climate.copernicus.eu/sectoral-applications-decadal-predictions"
+        target="_blank"
+        rel="noopener noreferrer"
+      >C3S website</a>.
+    </p>
   </div>
 </template>
