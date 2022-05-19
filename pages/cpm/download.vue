3ef5ba4a5afc8ee029d3c47312454620c606e2c5
Peter Kalverla <peter.kalverla@gmx.com>
2022-05-19 15:41:25 +0200

    link to tom's zenodo data

--- pages/cpm/download.vue
+++ pages/cpm/download.vue
@@ -7,11 +7,17 @@
     </p>
 
     <p>
-      We are looking into possibilities of making pre-processed subsets of the data available through zenodo:
+      In the meantime, we are making pre-processed subsets of the
+      data available through Zenodo. So far, we have the following:
     </p>
     <ul>
-      <li>Link 1</li>
-      <li>Link 2</li>
+      <li>
+        Gridded and global temperature and precipitation anomalies, for European domains and the Caribbean, including figures: <a
+          href="https://doi.org/10.5281/zenodo.6524935"
+          target="_blank"
+          rel="noopener noreferrer"
+        >https://doi.org/10.5281/zenodo.6524935</a>
+      </li>
     </ul>
   </div>
 </template>
